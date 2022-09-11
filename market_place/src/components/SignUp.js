7e5f77281101850c31a1b253d00065880bbd0934
mmulahvictor <mmulahvictor@gmail.com>
2022-09-11 18:21:37 +0300

    create a readme

---
+++ market_place/src/components/SignUp.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function SignUp() {
+  return (
+      <div>
+          <form action="">
+              <input type="text" />
+              <input type="text" />
+              <input type="text" />
+          </form>
+    </div>
+  )
+}
+
+export default SignUp
