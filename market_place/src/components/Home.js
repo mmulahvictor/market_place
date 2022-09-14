86190e086ffe175682354d8d220af67d552541be
mmulahvictor <mmulahvictor@gmail.com>
2022-09-14 11:11:14 +0300

    style the page with fonts and colors

--- market_place/src/components/Home.js
+++ market_place/src/components/Home.js
@@ -6,9 +6,8 @@ function Home () {
         <>
             <h1 className='title'>Raw Farm products</h1>
             <div className='div2'>
-                <h3>Sell your farm produce for FREE</h3>
-                <h4>Raw Farm Products is a marketplace you can trust</h4>
-                <p id='text'>Raw Farm Products is a FREE online marketplace where buyers and sellers meet to trade farm related products and services.</p>
+                <h3>Sell your farm produce for <span>FREE</span></h3>
+                <p id='text'>Raw Farm Products is a <span>FREE</span> online marketplace where buyers and sellers meet to trade farm related products and services.</p>
             </div>
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWOfUVjPLPaUqTls5_R52dkaxL0ob4N9rAUg&usqp=CAU" alt="farm products" className='div3' />
         </>
